301c30cc529665b9fcf71531651a3e0c819c095b
Preafericitul_Marcus <marcus888coman@gmail.com>
2025-10-27 23:36:01 +0200

    chore(#4): added User entity.

---
+++ backend/src/main/java/com/pharmacy/Pharmacy_Manager/model/User.java
@@ -0,0 +1,35 @@
+package com.pharmacy.Pharmacy_Manager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Builder
+@Entity
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Table(name = "users")
+public class User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(updatable = false, nullable = false,  unique = true)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    private String lastName;
+
+    @Column(nullable = false)
+    private String secondName;
+}
