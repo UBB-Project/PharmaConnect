610ccafe3cefeea4c6d85aff186441545d396833
Rares Cojocaru <161442085+rarescojocaru504@users.noreply.github.com>
2025-10-30 13:29:49 +0200

    Fix formatting issues in ItemController.java

--- backend/src/main/java/com/pharmacy/Pharmacy_Manager/controller/ItemController.java
+++ backend/src/main/java/com/pharmacy/Pharmacy_Manager/controller/ItemController.java
@@ -1,11 +1,11 @@
 package com.pharmacy.Pharmacy_Manager.controller;
+
 import com.pharmacy.Pharmacy_Manager.dto.ItemDTO;
 import com.pharmacy.Pharmacy_Manager.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.UUID;
 
 @RestController
