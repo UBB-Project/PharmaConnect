9c60adb08e8e7474124d309f22e10a5bc3165f92
Rares Cojocaru <161442085+rarescojocaru504@users.noreply.github.com>
2025-10-30 13:31:18 +0200

    Add missing import statement in ItemRepository

--- backend/src/main/java/com/pharmacy/Pharmacy_Manager/repository/ItemRepository.java
+++ backend/src/main/java/com/pharmacy/Pharmacy_Manager/repository/ItemRepository.java
@@ -1,4 +1,5 @@
 package com.pharmacy.Pharmacy_Manager.repository;
+
 import com.pharmacy.Pharmacy_Manager.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
