557145db830c8339430174ad3a2288317206c357
Andrada <ionutandrada31@yahoo.com>
2025-11-08 21:38:21 +0200

    fix PR x2

--- backend/src/main/java/com/pharmacy/Pharmacy_Manager/controller/OrderController.java
+++ backend/src/main/java/com/pharmacy/Pharmacy_Manager/controller/OrderController.java
@@ -6,7 +6,12 @@ import com.pharmacy.Pharmacy_Manager.model.Order;
 import com.pharmacy.Pharmacy_Manager.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.UUID;
