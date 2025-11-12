29d4409a07d58716695fb930796612e0c64aceec
Preafericitul_Marcus <marcus888coman@gmail.com>
2025-11-12 11:48:53 +0200

    feature(#15): first demo of the main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import LoginPage from "./pages/LoginPage/LoginPage.jsx";
+import SimpleSlider from "./components/Carousel/SimpleSlider.jsx";
 import "./App.css";
 import {useTranslation} from "react-i18next";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
     const { t } = useTranslation("home");
@@ -19,6 +22,7 @@ function App() {
                             <div className="home-page">
                                 <h1>{t("home.welcome")}</h1>
                                 <p>{t("home.tagline")}</p>
+                                <SimpleSlider />
                             </div>
                         }
                     />
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n.js";
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
