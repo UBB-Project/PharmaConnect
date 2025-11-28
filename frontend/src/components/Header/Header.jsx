94e67721a43d9e17f45e2e4de254cd4787367703
Preafericitul_Marcus <marcus888coman@gmail.com>
2025-11-28 12:54:10 +0200

    fix: header was not good

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import Popup from "../Popup/Popup.jsx";
 import "./Header.css";
 import { useNavigate, useLocation } from "react-router-dom";
 import ThemeSwitcher from "../Theme/ThemeSwitcher.jsx";
+import { Dropdown } from 'primereact/dropdown';
 import { Button } from 'primereact/button';
 
 export default function Header() {
@@ -21,25 +22,47 @@ export default function Header() {
         }
     };
 
-    const changeLanguage = (lng) => i18n.changeLanguage(lng);
+    const languages = [
+        { label: 'English', value: 'en', code: 'gb' },
+        { label: 'Română', value: 'ro', code: 'ro' }
+    ];
+
+    const changeLanguage = (e) => {
+        i18n.changeLanguage(e.value);
+    };
+
+    const countryOptionTemplate = (option) => {
+        if (!option) {
+            return <span>Select</span>;
+        }
+
+        return (
+            <div className="language-item">
+                <span className={`fi fi-${option.code}`}></span>
+                <span>{option.label}</span>
+            </div>
+        );
+    };
+
+    const currentVal = languages.find(l => l.value === i18n.language) ? i18n.language : languages[0].value;
 
     return (
         <header className="header">
             <div className="header-left">
-                <div className="select-wrapper">
-                    <select
-                        value={i18n.language}
-                        onChange={(e) => changeLanguage(e.target.value)}
-                        aria-label="Select Language"
-                    >
-                        <option value="en">English</option>
-                        <option value="ro">Română</option>
-                    </select>
-                </div>
+                <Dropdown
+                    value={currentVal}
+                    onChange={changeLanguage}
+                    options={languages}
+                    optionLabel="label"
+                    valueTemplate={countryOptionTemplate}
+                    itemTemplate={countryOptionTemplate}
+                    className="language-dropdown"
+                    panelClassName="language-dropdown-panel"
+                    aria-label="Select Language"
+                />
             </div>
 
-            <h1 className="header-title">PharmaConnect</h1>
-
+            <h1 className="header-title" onClick={() => navigate("/")} >PharmaConnect</h1>
 
             <div className="header-right">
                 <ThemeSwitcher />
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,6 +11,7 @@ import 'primereact/resources/themes/lara-light-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
+import "flag-icons/css/flag-icons.min.css";
 
 
 
