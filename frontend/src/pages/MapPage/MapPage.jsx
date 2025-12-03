db9940d90d7c2ba8604a0dd50d8241df7ccd48cc
rarescojocaru504 <rares.cojocaru@stud.ubbcluj.ro>
2025-12-03 10:31:33 +0200

    Removed button from mappage

--- frontend/src/pages/MapPage/MapPage.jsx
+++ frontend/src/pages/MapPage/MapPage.jsx
@@ -1,25 +1,9 @@
 import PharmacyMap from "../../components/Map/PharmacyMap.jsx";
-import { Link } from "react-router-dom";
-import { Button } from 'primereact/button';
-import { useTranslation } from "react-i18next";
 import "./MapPage.css";
 
 export default function MapPage() {
-    const { t } = useTranslation("home");
-
     return (
         <div className="map-page-container">
-            <div className="map-navigation">
-                <Link to="/" className="nav-link">
-                    <Button
-                        label={t("home.back") || "Back to Home"}
-                        icon="pi pi-arrow-left"
-                        className="super-btn-primary"
-                        rounded
-                    />
-                </Link>
-            </div>
-
             <div className="map-content">
                 <PharmacyMap />
             </div>
