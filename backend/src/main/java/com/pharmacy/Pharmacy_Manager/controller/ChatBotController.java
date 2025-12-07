2971805eb9b91df922b31fa6b48730b1d1bf8de3
Jorj-Raluca <Jorj.Raluca@ibm.com>
2025-12-07 11:43:26 +0200

    ui fixes for header and carousel

--- backend/src/main/java/com/pharmacy/Pharmacy_Manager/controller/ChatBotController.java
+++ backend/src/main/java/com/pharmacy/Pharmacy_Manager/controller/ChatBotController.java
@@ -1,28 +1,28 @@
-package com.pharmacy.Pharmacy_Manager.controller;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.pharmacy.Pharmacy_Manager.dto.PromptRequest;
-import com.pharmacy.Pharmacy_Manager.service.ChatBotService;
-
-import org.springframework.web.bind.annotation.RequestBody;
-
-@CrossOrigin("*")
-@RestController
-@RequestMapping("/api/chat")
-public class ChatBotController {
-
-  private final ChatBotService chatBotService;
-
-  public ChatBotController(ChatBotService chatBotService) {
-    this.chatBotService = chatBotService;
-  }
-
-  @PostMapping
-  public String chat(@RequestBody PromptRequest promptRequest) {
-    return chatBotService.getChatResponse(promptRequest);
-  }
-}
+//package com.pharmacy.Pharmacy_Manager.controller;
+//
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.pharmacy.Pharmacy_Manager.dto.PromptRequest;
+//import com.pharmacy.Pharmacy_Manager.service.ChatBotService;
+//
+//import org.springframework.web.bind.annotation.RequestBody;
+//
+//@CrossOrigin("*")
+//@RestController
+//@RequestMapping("/api/chat")
+//public class ChatBotController {
+//
+//  private final ChatBotService chatBotService;
+//
+//  public ChatBotController(ChatBotService chatBotService) {
+//    this.chatBotService = chatBotService;
+//  }
+//
+//  @PostMapping
+//  public String chat(@RequestBody PromptRequest promptRequest) {
+//    return chatBotService.getChatResponse(promptRequest);
+//  }
+//}
